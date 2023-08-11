813fe7e567be2e6182bfd8b4d9f121ee3e2f73d1
Joas-Assuncao <joassalvior@gmail.com>
2023-08-11 09:14:58 -0300

    add loading when is calling api, add logical in input search records and order by too

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,50 +1,110 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useMemo, useState } from 'react';
 import {
-    Card, Container, Header, InputSearchContainer, ListContainer,
+    Card, Container, Header, InputSearchContainer, ListHeader,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-// import { Loader } from '../../components/Loader';
+import { Loader } from '../../components/Loader';
 // import { Modal } from '../../components/Modal';
 
 export function Home() {
+    const [contacts, setContacts] = useState([]);
+    const [orderBy, setOrderBy] = useState('asc');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+
+    const filteredContacts = useMemo(() => (
+        contacts.filter((contact) => contact.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    ), [contacts, searchTerm]);
+
+    useEffect(() => {
+        async function loadContacts() {
+            try {
+                setIsLoading(true);
+
+                const response = await fetch(
+                    `http://localhost:3001/contacts?orderBy=${orderBy}`,
+                );
+
+                const json = await response.json();
+
+                setContacts(json);
+            } catch(error) {
+                console.error(error);
+            }
+            finally {
+                setIsLoading(false);
+            }
+        }
+
+        loadContacts();
+    }, []);
+
+    function handleToggleOrderBy() {
+        setOrderBy(
+            (prevState) => (prevState === 'asc' ? 'desc' : 'asc'),
+        );
+    }
+
+    function handleChangeSearchTerm(event) {
+        const valueSearchTerm = event.target.value;
+
+        setSearchTerm(valueSearchTerm);
+    }
+
     return (
         <Container>
-            {/* <Loader /> */}
+            <Loader isLoading={isLoading} />
+
             {/* <Modal danger /> */}
             <InputSearchContainer>
-                <input type="text" placeholder="Pesquise pelo nome..." />
+                <input
+                    value={searchTerm}
+                    type="text"
+                    placeholder="Pesquise pelo nome..."
+                    onChange={handleChangeSearchTerm}
+                />
             </InputSearchContainer>
 
             <Header>
-                <strong>3 Contatos</strong>
+                <strong>
+                    {filteredContacts.length}
+                    {filteredContacts.length === 1 ? ' Contato' : ' Contatos'}
+                </strong>
                 <Link to="/new">
                     Novo contato
                 </Link>
             </Header>
 
-            <ListContainer>
-                <header>
-                    <button type="button" className="sort-button">
-                        <span>Nome</span>
-                        <img src={arrow} alt="Arrow" />
-                    </button>
-                </header>
+            {
+                !!filteredContacts.length && (
+                    <ListHeader orderBy={orderBy}>
+                        <button type="button" className="sort-button" onClick={handleToggleOrderBy}>
+                            <span>Nome</span>
+                            <img src={arrow} alt="Arrow" />
+                        </button>
+                    </ListHeader>
+                )
+            }
 
-                <Card>
+            {filteredContacts.map((contact) => (
+                <Card key={contact.id}>
                     <div className="info">
                         <div className="contact-info">
-                            <strong>Joas Assunção</strong>
-                            <small>Instagram</small>
+                            <strong>{contact.name}</strong>
+                            {contact.category_name && (
+                                <small>{contact.category_name}</small>
+                            )}
                         </div>
-                        <span>joassalvior@gmail.com</span>
-                        <span>(61) 9 8989-9898</span>
+                        <span>{contact.email}</span>
+                        <span>{contact.phone}</span>
                     </div>
 
                     <div className="actions">
-                        <Link to="/edit/1">
+                        <Link to={`/edit/${contact.id}`}>
                             <img src={edit} alt="Editar" />
                         </Link>
                         <button type="button">
@@ -52,47 +112,7 @@ export function Home() {
                         </button>
                     </div>
                 </Card>
-
-                <Card>
-                    <div className="info">
-                        <div className="contact-info">
-                            <strong>Joas Assunção</strong>
-                            <small>Instagram</small>
-                        </div>
-                        <span>joassalvior@gmail.com</span>
-                        <span>(61) 9 8989-9898</span>
-                    </div>
-
-                    <div className="actions">
-                        <Link to="/edit/2">
-                            <img src={edit} alt="Editar" />
-                        </Link>
-                        <button type="button">
-                            <img src={trash} alt="Deletar" />
-                        </button>
-                    </div>
-                </Card>
-
-                <Card>
-                    <div className="info">
-                        <div className="contact-info">
-                            <strong>Joas Assunção</strong>
-                            <small>Instagram</small>
-                        </div>
-                        <span>joassalvior@gmail.com</span>
-                        <span>(61) 9 8989-9898</span>
-                    </div>
-
-                    <div className="actions">
-                        <Link to="/edit/3">
-                            <img src={edit} alt="Editar" />
-                        </Link>
-                        <button type="button">
-                            <img src={trash} alt="Deletar" />
-                        </button>
-                    </div>
-                </Card>
-            </ListContainer>
+            ))}
         </Container>
     );
 }
