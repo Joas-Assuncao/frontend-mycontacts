ec0be43f70cde89b7658b3f456afeecc68260b65
Joas-Assuncao <joassalvior@gmail.com>
2023-08-17 10:57:21 -0300

    add gray 100 and adjust loading overlay

--- src/components/Loader/styles.js
+++ src/components/Loader/styles.js
@@ -44,7 +44,7 @@ export const Overlay = styled.div`
 
     background: rgba(246, 245, 252, 0.7);
 
-    position: absolute;
+    position: fixed;
     left: 0;
     top: 0;
 
--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -10,7 +10,7 @@ export const Overlay = styled.div`
 
     background: rgba(0, 0, 0, 0.06);
 
-    position: absolute;
+    position: fixed;
     left: 0;
     top: 0;
 
