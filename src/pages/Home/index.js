1b88cfc43ce3f336bfa9b9c10d6dbc3f694c8c12
Joas-Assuncao <joassalvior@gmail.com>
2023-11-03 09:26:10 -0300

    feat: created delete contact logical, refactor contacts called

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,28 @@
-import { useEffect, useMemo, useState } from 'react';
+import {
+    useCallback,
+    useEffect,
+    useMemo,
+    useState
+} from 'react';
 import { Link } from 'react-router-dom';
+
 import {
     Card, Container, ErrorContainer, Header, InputSearchContainer, ListHeader
 } from './styles';
-
-import { Button } from '../../components/Button';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import sad from '../../assets/images/icons/sad.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
+import { Button } from '../../components/Button';
 import { Loader } from '../../components/Loader';
-import ContactsService from '../../services/ContactsService';
+import { Modal } from '../../components/Modal';
+
 import { delay } from '../../utils/delay';
+import { toast } from '../../utils/toast';
 import { formatPhone } from '../../utils/formatPhone';
-// import { Modal } from '../../components/Modal';
+import ContactsService from '../../services/ContactsService';
 
 export function Home() {
     const [contacts, setContacts] = useState([]);
@@ -23,12 +30,15 @@ export function Home() {
     const [searchTerm, setSearchTerm] = useState('');
     const [isLoading, setIsLoading] = useState(true);
     const [hasError, setHasError] = useState(false);
+    const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+    const [contactBeingDeleted, setContactBeingDeleted] = useState(null);
+    const [isLoadingDelete, setIsLoadingDelete] = useState(false);
 
     const filteredContacts = useMemo(() => (
         contacts.filter((contact) => contact.name.toLowerCase().includes(searchTerm.toLowerCase()))
     ), [contacts, searchTerm]);
 
-    async function loadContacts() {
+    const loadContacts = useCallback(async () => {
         try {
             setIsLoading(true);
             await delay(1500);
@@ -44,11 +54,11 @@ export function Home() {
         } finally {
             setIsLoading(false);
         }
-    }
+    }, [orderBy]);
 
     useEffect(() => {
         loadContacts();
-    }, [orderBy]);
+    }, [orderBy, loadContacts]);
 
     function handleToggleOrderBy() {
         setOrderBy(
@@ -66,15 +76,64 @@ export function Home() {
         loadContacts();
     }
 
+    function handleDeleteContact(contact) {
+        setIsDeleteModalVisible(true);
+
+        setContactBeingDeleted(contact);
+    }
+
+    function handleCloseDeleteModal() {
+        setIsDeleteModalVisible(false);
+        setContactBeingDeleted(null);
+    }
+
+    async function handleConfirmDeleteContact() {
+        try {
+            setIsLoadingDelete(true);
+
+            await ContactsService.deleteContact(contactBeingDeleted.id);
+
+            setContacts((prevState) => (
+                prevState.filter((contact) => contact.id !== contactBeingDeleted.id)
+            ));
+
+            toast({
+                type: 'success',
+                text: 'Contato deletado com sucesso!',
+                duration: 1000,
+            });
+
+            handleCloseDeleteModal();
+        } catch (err) {
+            toast({
+                type: 'danger',
+                text: 'Ocorreu um erro ao deletar o contato!',
+                duration: 3000,
+            });
+        } finally {
+            setIsLoadingDelete(false);
+        }
+    }
+
     return (
         <Container>
             <Loader isLoading={isLoading} />
 
-            {/* <Modal danger /> */}
+            <Modal
+                danger
+                confirmLabel="Deletar"
+                visible={isDeleteModalVisible}
+                onCancel={handleCloseDeleteModal}
+                isLoading={isLoadingDelete}
+                onConfirm={handleConfirmDeleteContact}
+                title={`Tem certeza que deseja deletar o contato "${contactBeingDeleted?.name}"?`}
+            >
+                <p>Esta ação não poderá ser desfeita!</p>
+            </Modal>
             <InputSearchContainer>
                 <input
-                    value={searchTerm}
                     type="text"
+                    value={searchTerm}
                     placeholder="Pesquise pelo nome..."
                     onChange={handleChangeSearchTerm}
                 />
@@ -114,7 +173,11 @@ export function Home() {
                         {
                             filteredContacts.length > 0 && (
                                 <ListHeader orderBy={orderBy}>
-                                    <button type="button" className="sort-button" onClick={handleToggleOrderBy}>
+                                    <button
+                                        type="button"
+                                        className="sort-button"
+                                        onClick={handleToggleOrderBy}
+                                    >
                                         <span>Nome</span>
                                         <img src={arrow} alt="Arrow" />
                                     </button>
@@ -140,7 +203,10 @@ export function Home() {
                                         <Link to={`/edit/${contact.id}`}>
                                             <img src={edit} alt="Editar" />
                                         </Link>
-                                        <button type="button">
+                                        <button
+                                            type="button"
+                                            onClick={() => handleDeleteContact(contact)}
+                                        >
                                             <img src={trash} alt="Deletar" />
                                         </button>
                                     </div>
